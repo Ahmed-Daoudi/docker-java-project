45594e03b769feb9759d30150f3ae2babc56a161
Ahmed-Daoudi <ahmeddaoudi386@gmail.com>
2023-02-23 18:14:25 -0500

    java-application

--- src/main/resources/db_backup.sql
+++ src/main/resources/db_backup.sql
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `role`
 --
+--
+-- Table structure for table `role`
+--
 
 DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
